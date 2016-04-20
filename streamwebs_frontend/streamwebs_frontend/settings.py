7a10bda193c932d6829e38322feaefbcf50b0214
Aileen Thai <thai@osuosl.org>
2016-04-20 13:30:51 -0700

    Postgis

--- streamwebs_frontend/streamwebs_frontend/settings.py
+++ streamwebs_frontend/streamwebs_frontend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'streamwebs',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'streamwebs_frontend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'streamwebs',
+        'USER': 'postgres',
     }
 }
 
