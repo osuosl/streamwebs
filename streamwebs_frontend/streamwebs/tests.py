68f71bdfa3f4ecdbcf73313b73fe1bfa0793ff4e
leian7 <leian@osuosl.org>
2016-05-04 10:12:57 -0700

    add index test

--- streamwebs_frontend/streamwebs/tests.py
+++ streamwebs_frontend/streamwebs/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/streamwebs/')
+        self.assertEqual(response.status_code, 200)
