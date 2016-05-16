7230483cd718455b5205893e36e84aa1d5e85bc7
Aileen Thai <thai@osuosl.org>
2016-05-16 14:56:58 -0700

    Fix tests

--- streamwebs_frontend/streamwebs/tests/models/test_site_model.py
+++ streamwebs_frontend/streamwebs/tests/models/test_site_model.py
@@ -2,14 +2,16 @@ from django.test import TestCase
 
 from streamwebs.models import Site
 from django.contrib.gis.db import models
+from django.apps import apps
+from itertools import chain
 
 
 class SiteTestCase(TestCase):
 
     def setUp(self):
         self.expected_fields = {
-            'site_name': models.TextField,
-            'site_type': models.TextField,
+            'site_name': models.CharField,
+            'site_type': models.CharField,
             'description': models.TextField,
             'site_slug': models.SlugField,  # Note: max_length defaults to 50
             'location': models.PointField,
@@ -23,29 +25,29 @@ class SiteTestCase(TestCase):
         }
 
     def test_fields_exist(self):
-        model = models.get_model('streamwebs', 'site')
+        model = apps.get_model('streamwebs', 'site')
         for field, field_type in self.expected_fields.items():
             self.assertEqual(
-                field_type, type(model._meta.get_field_by_name(field[0])))
+                field_type, type(model._meta.get_field(field)))
 
     def test_no_extra_fields(self):
-        fields = Site._meta.get_fields()
+        # the following is equivalent to MyField._meta.get_all_field_names()
+        # which was deprecated in Django 1.9
+        fields = list(set(chain.from_iterable(
+            (field.name, field.attname) if hasattr(field, 'attname') else
+            (field.name,) for field in Site._meta.get_fields()
+            if not (field.many_to_one and field.realted_model is None)
+        )))
         self.assertEqual(sorted(fields), sorted(self.expected_fields.keys()))
 
     def test_optional_fields(self):
-        models.get_model('streamwebs', 'site')
+        apps.get_model('streamwebs', 'site')
         for field in self.optional_fields:
             self.assertEqual(
-                Site._meta.get_field_by_name(field)[0].blank, True)
+                Site._meta.get_field(field).blank, True)
 
     def test_create_and_mod_dates(self):
         """When a new site is created, both date fields should be set"""
         self.assertTrue(Site._meta.get_field('modified').auto_now)
         self.assertTrue(Site._meta.get_field('created').auto_now_add)
-
-#    def test_modify_date(self):
-#        """Test that only the 'modified' date changes when site info is
-#           updated"""
-#        self.assertTrue(Site._meta_.get_field('modified').auto_now)
-#        self.assertEqual()
 
