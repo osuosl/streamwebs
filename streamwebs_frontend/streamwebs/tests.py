ab3c25bc465b6d2f8df56ec16176ec0bcf645f45
leian7 <leian@osuosl.org>
2016-05-16 10:42:37 -0700

    Remove index test; wrong PR

--- streamwebs_frontend/streamwebs/tests.py
+++ streamwebs_frontend/streamwebs/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
-class IndexViewTest(TestCase):
-    def test_index(self):
-        response = self.client.get('/streamwebs/')
-        self.assertEqual(response.status_code, 200)
+
