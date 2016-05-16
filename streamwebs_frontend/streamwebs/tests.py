3f865582d943bb2a8cdc99e1d3d96e720080b917
leian7 <leian@osuosl.org>
2016-05-16 13:32:51 -0700

    test if birthdate valid

--- streamwebs_frontend/streamwebs/tests.py
+++ streamwebs_frontend/streamwebs/tests.py
@@ -1,5 +1,7 @@
+import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
+
 
 #from streamwebs.models import UserProfile
 
@@ -8,8 +10,8 @@ from django.contrib.auth.models import User
 class UserTestCase(TestCase):
 
     def test_UserProfile_objs_exist(self):
-        profile = UserProfile.objects.create('School', birthdate=date(1999, 4,
-            1))
+        profile = UserProfile.objects.create('School',
+                birthdate=datetime.date(1999, 4, 1))
         self.assertEqual(('profile' in locals()), True)
         
     def test_User_UserProfile_OneToOne(self):
@@ -19,8 +21,8 @@ class UserTestCase(TestCase):
         django_user.last_name = 'User'
         django_user.save()
 
-        custom_user = UserProfile(school='Custom School', birthdate=date(1990,
-            5, 12))
+        custom_user = UserProfile(school='Custom School',
+                birthdate=datetime.date(1990, 5, 12))
         custom_user = django_user
         custom_user.save()
 
@@ -31,4 +33,23 @@ class UserTestCase(TestCase):
         self.assertEqual(custom_user.last_name, 'User')
         
         self.assertEqual(django_user.school, 'Custom School')
-        self.assertEqual(django_user.birthdate, date(1990, 5, 12)) 
+        self.assertEqual(django_user.birthdate, datetime.date(1990, 5, 12))
+
+    def test_younger_than_13(self):
+        now = datetime.datetime.now()
+        bad_year_date = datetime.date(now.year - 12, now.month, now.day)
+        self.assertEqual(is_valid_birthdate(bad_year_date), False)
+
+        bad_month_date = datetime.date(now.year - 13, now.month + 1, now.day)
+        self.assertEqual(is_valid_birthdate(bad_month_date), False)
+
+        bad_day_date = datetime.date(now.year - 13, now.month, now.day+1)
+        self.assertEqual(is_valid_birthdate(bad_day_date), False)
+
+    def test_13_or_older(self):
+        now = datetime.datetime.now()
+        thirteen_today = datetime.date(now.year-13, now.month, now.day)
+        self.assertEqual(is_valid_birthdate(thirteen_today), True)
+
+        older_than_13 = datetime.date(now.year - 14, now.month, now.day)
+        self.assertEqual(is_valid_birthdate(older_than_13), True)
