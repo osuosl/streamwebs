27017d1e07ef6a6726040a7c2bb7ff24f9f2c69c
Tristan Patch <patcht@osuosl.org>
2016-05-16 16:24:21 -0700

    Added basic sites view (no CSS).

--- streamwebs_frontend/streamwebs/views.py
+++ streamwebs_frontend/streamwebs/views.py
@@ -4,4 +4,37 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey, you've reached the index.")
+    return render(request, 'streamwebs/index.html', {})
+
+
+def sites(request):
+    # TODO: actual lookup
+    site_list = [
+        {
+            'name': 'Test 1',
+            'slug': 'test1',
+            'site_type': 'steward',
+            'location': {'x': -122.31211423, 'y': 45.11231324}
+        },
+        {
+            'name': 'Test 2',
+            'slug': 'test2',
+            'site_type': 'salmon',
+            'location': {'x': -121.78305424, 'y': 44.74928593}
+        },
+        {
+            'name': 'Other One',
+            'slug': 'other',
+            'site_type': 'available',
+            'location': {'x': -122.00193922, 'y': 45.01835423}
+        },
+        {
+            'name': 'Me too',
+            'slug': 'me2',
+            'site_type': 'salmon',
+            'location': {'x': -121.93729485, 'y': 45.62930212}
+        }
+    ]
+    return render(request, 'streamwebs/sites.html', {
+        'sites': site_list,
+    })
