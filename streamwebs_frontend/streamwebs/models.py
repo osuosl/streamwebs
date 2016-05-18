eb025ea1d12cd8ec301d00ad4a1745dfb373e8e3
leian7 <leian@osuosl.org>
2016-05-18 13:11:24 -0700

    start writing UserProfile model

--- streamwebs_frontend/streamwebs/models.py
+++ streamwebs_frontend/streamwebs/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    school = models.CharField(max_length=140, blank=True)
+    birthdate = models.DateField('birthdate')
+
+    def is_valid_birthdate(self):
+        if self.birthdate.year > 2003:
+            return False
+
+    def __unicode__(self):
+        return self.user.username
