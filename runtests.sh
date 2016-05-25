508eb478fcf1e0163cf99a5eb911ed349d748b1a
Aileen Thai <thai@osuosl.org>
2016-05-25 14:21:39 -0700

    Set env variables + debugging

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,3 @@
 python streamwebs_frontend/manage.py makemigrations
 python streamwebs_frontend/manage.py migrate
 python streamwebs_frontend/manage.py test streamwebs_frontend/streamwebs/tests/models
-python streamwebs_frontend/manage.py test streamwebs_frontend/streamwebs/tests/views
