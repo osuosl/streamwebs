add1bb754e0799ce2928c5bc7329f3f35a52c7be
Aileen Thai <thai@osuosl.org>
2016-06-01 14:50:58 -0700

    Rough tests

--- streamwebs_frontend/streamwebs/tests/models/test_water_quality.py
+++ streamwebs_frontend/streamwebs/tests/models/test_water_quality.py
@@ -1,17 +1,21 @@
 from django.test import TestCase
 
-from streamwebs.models import Water_Quality
 from django.contrib.gis.db import models
 from django.apps import apps
 from itertools import chain
+from django.core.exceptions import ValidationError
+
+from streamwebs.models import Site, SiteManager, validate_wq_site
+from streamwebs.models import Water_Quality
+
 
 class WaterQualityTestCase(TestCase):
 
     def setUp(self):
         self.expected_fields = {
-            'site_name': models.ForeignKey,
+            'site': models.ForeignKey,
             'date': models.DateField,
-            'time': models.TimeField, 
+            # 'time': models.TimeField,
             'school': models.CharField,
             'teacher': models.CharField,
             'latitude': models.DecimalField,
@@ -24,6 +28,16 @@ class WaterQualityTestCase(TestCase):
             'dissolved_O2': models.DecimalField,
             'pH': models.DecimalField,
             'turbidity': models.DecimalField,
+            'salinity': models.DecimalField,
+            'conductivity': models.DecimalField,
+            'total_solids': models.DecimalField,
+            'bod': models.DecimalField,
+            'ammonia': models.DecimalField,
+            'nitrite': models.DecimalField,
+            'nitrate': models.DecimalField,
+            'phosphates': models.DecimalField,
+            'fecal_coliform': models.DecimalField,
+            'notes': models.TextField,
             'id': models.AutoField
         }
 
@@ -32,8 +46,15 @@ class WaterQualityTestCase(TestCase):
         # Sample number??
 
         self.optional_fields = {
-            'live_fish',
-            'dead_fish'
+            'conductivity',
+            'total_solids',
+            'bod',
+            'ammonia',
+            'nitrite',
+            'nitrate',
+            'phosphate',
+            'fecal_coliform',
+            'notes',
         }
 
     def test_fields_exist(self):
@@ -42,9 +63,9 @@ class WaterQualityTestCase(TestCase):
             self.asserEqual(
                 field_type, type(model._meta.get_field(field)))
 
-    def test_no_extra fields(self):
+    def test_no_extra_fields(self):
         fields = list(set(chain.from_iterable(
-            (field_name, field.attname) if hasattr(field, 'attname') else
+            (field.name, field.attname) if hasattr(field, 'attname') else
             (field.name,) for field in Water_Quality._meta.get_fields()
             if not (field.many_to_one and field.related_model is None)
         )))
@@ -57,5 +78,45 @@ class WaterQualityTestCase(TestCase):
                 Water_Quality._meta.get_field(field).blank, True)
 
     def test_datasheet_OneToOne(self):
-        site = Site.objects.create_site('name', 'type', 'slug');
-        waterq = 
+        site = Site.objects.create_site('test', 'some_type', 'some_slug')
+        waterq = Water_Quality.objects.create(site=site, date='2016-06-01',
+                                              school='a', teacher='teacher',
+                                              latitude=123, longitude=123,
+                                              fish_present='False',
+                                              live_fish=0, dead_fish=0,
+                                              water_temp=45, air_temp=65,
+                                              dissolved_O2=15, pH=6.7,
+                                              turbidity=0, salinity=12)
+        # waterq = apps.get_model('streamwebs', 'water_quality')
+        self.assertEqual(waterq.site.site_name, 'test')
+        # check for a match in lat and long?
+
+    def test_datasheet_nonexistent_site(self):
+        # site = Site.objects.create('test', 'some_type', 'some_slug')
+        no_site_wq = Water_Quality.objects.create(site='nonexistent',
+                                                  date='2016-06-01',
+                                                  school='a',
+                                                  teacher='teacher',
+                                                  latitude=123, longitude=123,
+                                                  fish_present='False',
+                                                  live_fish=0, dead_fish=0,
+                                                  water_temp=45, air_temp=65,
+                                                  dissolved_O2=15, pH=6.7,
+                                                  turbidity=0, salinity=12)
+        with self.assertRaises(ValidationError):
+            validate_wq_site(no_site_wq.site_name)
+
+    def test_datasheet_bad_site(self):
+        # site = Site.objects.create('test', 'some_type', 'some_slug')
+        bad_site_wq = Water_Quality.objects.create(site='#@^%--&',
+                                                   date='2016-06-01',
+                                                   school='a',
+                                                   teacher='teacher',
+                                                   latitude=123, longitude=123,
+                                                   fish_present='False',
+                                                   live_fish=0, dead_fish=0,
+                                                   water_temp=45, air_temp=65,
+                                                   dissolved_O2=15, pH=6.7,
+                                                   turbidity=0, salinity=12)
+        with self.assertRaises(ValidationError):
+            validate_wq_site(bad_site_wq.site_name)
