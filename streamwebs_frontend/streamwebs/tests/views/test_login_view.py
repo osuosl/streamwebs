3f772392ae3ee6e0dc96c8bb511daf2c98bd9a5d
leian7 <leian@osuosl.org>
2016-06-09 14:36:27 -0700

    flake 8 and setUp modification

--- streamwebs_frontend/streamwebs/tests/views/test_login_view.py
+++ streamwebs_frontend/streamwebs/tests/views/test_login_view.py
@@ -2,13 +2,12 @@ from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
-from streamwebs.models import UserProfile
 
 class LoginTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
-        user = User.objects.create_user(
+        self.user = User.objects.create_user(
             'john',
             'john@example.com',
             'johnpassword'
@@ -24,17 +23,34 @@ class LoginTestCase(TestCase):
         self.assertTemplateUsed(response, 'streamwebs/login.html')
 
     def test_good_username_good_password(self):
-        response_good = self.client.post(reverse('streamwebs:login'), {'username': 'john', 'password': 'johnpassword'})
-        self.assertRedirects(response_good, reverse('streamwebs:index'), status_code=302, target_status_code=200)
+        response_good = self.client.post(
+            reverse('streamwebs:login'),
+            {'username': 'john', 'password': 'johnpassword'}
+        )
+        self.assertRedirects(
+            response_good,
+            reverse('streamwebs:index'),
+            status_code=302,
+            target_status_code=200
+        )
 
     def test_good_username_bad_password(self):
-        response_bad_pass = self.client.post(reverse('streamwebs:login'), {'username': 'john', 'password': 'badpassword'})
+        response_bad_pass = self.client.post(
+            reverse('streamwebs:login'),
+            {'username': 'john', 'password': 'badpassword'}
+        )
         self.assertContains(response_bad_pass, 'Invalid credentials')
 
     def test_bad_username_good_password(self):
-        response_bad_name = self.client.post(reverse('streamwebs:login'), {'username': 'notjohn', 'password': 'johnpassword'})
+        response_bad_name = self.client.post(
+            reverse('streamwebs:login'),
+            {'username': 'notjohn', 'password': 'johnpassword'}
+        )
         self.assertContains(response_bad_name, 'Invalid credentials')
 
     def test_bad_username_bad_password(self):
-        response_both_bad = self.client.post(reverse('streamwebs:login'), {'username': 'notjohn', 'password': 'badpassword'})
+        response_both_bad = self.client.post(
+            reverse('streamwebs:login'),
+            {'username': 'notjohn', 'password': 'badpassword'}
+        )
         self.assertContains(response_both_bad, 'Invalid credentials')
