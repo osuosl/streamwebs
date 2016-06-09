8da7d9b70bbfe2c4e2c976aced1d7670db5e32c1
leian7 <leian@osuosl.org>
2016-06-09 15:13:53 -0700

    specify template

--- streamwebs_frontend/streamwebs/views.py
+++ streamwebs_frontend/streamwebs/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey, you've reached the index.")
+#    return HttpResponse("Hey, you've reached the index.")
+    return render_to_string('streamwebs/index.html')
