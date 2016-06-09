277931f887a5dac79d36bf50ba9144656032e825
leian7 <leian@osuosl.org>
2016-06-09 15:43:09 -0700

    Check that correct template is rendered

--- streamwebs_frontend/streamwebs/tests.py
+++ streamwebs_frontend/streamwebs/tests.py
@@ -5,3 +5,4 @@ class IndexViewTest(TestCase):
     def test_index(self):
         response = self.client.get('/streamwebs/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'streamwebs/index.html')
