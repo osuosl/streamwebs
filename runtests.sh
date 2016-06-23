d74db24ffdc734a776c7219dfaed2bc86d0d5652
leian7 <leian@osuosl.org>
2016-06-23 12:15:02 -0700

    add views back

--- runtests.sh
+++ runtests.sh
@@ -4,3 +4,4 @@
 python streamwebs_frontend/manage.py makemigrations
 python streamwebs_frontend/manage.py migrate
 python streamwebs_frontend/manage.py test streamwebs_frontend/streamwebs/tests/models
+python streamwebs_frontend/manage.py test streamwebs_frontend/streamwebs/tests/views
