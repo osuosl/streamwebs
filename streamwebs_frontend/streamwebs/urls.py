58c45db676f163e1d2eb54ed16e226b6c099b523
Tristan Patch <patcht@osuosl.org>
2016-06-28 15:38:02 -0700

    Initial implementation of water quality edit and view pages.

--- streamwebs_frontend/streamwebs/urls.py
+++ streamwebs_frontend/streamwebs/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^sites$', views.sites, name='sites'),
     url(r'^site/(?P<site_slug>[0-9a-zA-Z]+)$', views.site, name='site'),
+    url(r'^site/(?P<site_slug>[0-9a-zA-Z]+)/water/(?P<data_id>\d+)',
+        views.water_quality, name='water_quality'),
+    url(r'^site/(?P<site_slug>[0-9a-zA-Z]+)/water/edit',
+        views.water_quality_edit, name='water_quality_edit'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^logout/$', views.user_logout, name='logout')
 ]
--- streamwebs_frontend/streamwebs/views.py
+++ streamwebs_frontend/streamwebs/views.py
@@ -49,7 +49,25 @@ def sites(request):
 
 
 def site(request, site_slug):
-    return HttpResponse('Viewing page for site %s' % site_slug)
+    site = {
+        'name': 'Test 1',
+        'slug': 'test1',
+        'site_type': 'steward',
+        'location': {'x': -122.31211423, 'y': 45.11231324},
+        'data': {
+            'Water Quality': {
+                1: '2016-06-10',
+                2: '2016-06-12'
+            }
+        }
+    }
+    types = {
+        'water_quality_edit': 'Water Quality'
+    }
+    return render(request, 'streamwebs/site_detail.html', {
+        'site': site,
+        'data_types': types
+    })
 
 
 def register(request):
@@ -76,9 +94,9 @@ def register(request):
         profile_form = UserProfileForm()
 
     return render(request, 'streamwebs/register.html', {
-            'user_form': user_form,
-            'profile_form': profile_form,
-            'registered': registered})
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered})
 
 
 def user_login(request):
@@ -102,3 +120,127 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/streamwebs/')
+
+
+def water_quality(request, site_slug, data_id):
+    quality_data = {
+        'id': 1,
+        'date': '2016-06-10',
+        'site': {
+            'name': 'Test 1',
+            'slug': 'test1',
+            'site_type': 'steward',
+            'location': {'x': -122.31211423, 'y': 45.11231324}
+        },
+        'school': 'Education Middle School',
+        'DEQ_wq_level': 'B',
+        'fish_present': True,
+        'live_fish': 5,
+        'dead_fish': 2,
+        'air_temp_unit': 'F',
+        'water_temp_unit': 'C',
+        'sample_1': {
+            'water_temperature': 45,
+            'water_temp_tool': 'Vernier',
+            'air_temp': 52.3456,
+            'air_temp_tool': 'Manual',
+            'dissolved_oxygen': 55.2,
+            'oxygen_tool': 'Manual',
+            'pH': 6.2,
+            'pH_tool': 'Vernier',
+            'turbidity': 12.4,
+            'turbid_tool': 'Vernier',
+            'salinity': 5.8,
+            'salt_tool': 'Vernier',
+            'conductivity': 7.644,
+            'total_solids': .221,
+            'bod': 15.2,
+            'ammonia': 5.256,
+            'nitrite': 15.2,
+            'nitrate': 1.23,
+            'phosphates': 8.26,
+            'fecal_coliform': 54.2
+        },
+        'sample_2': {
+            'water_temp': 23,
+            'water_temp_tool': 'Vernier',
+            'air_temp': 123.346,
+            'air_temp_tool': 'Manual',
+            'dissolved_oxygen': 87.4,
+            'oxygen_tool': 'Manual',
+            'pH': 7.1,
+            'pH_tool': 'Vernier',
+            'turbidity': 3.2,
+            'turbid_tool': 'Manual',
+            'salinity': 1.558,
+            'salt_tool': 'Manual',
+            'conductivity': 84.14,
+            'total_solids': 1.32,
+            'bod': 1.223,
+            'ammonia': 1.223,
+            'nitrite': 8.36,
+            'nitrate': 9.56,
+            'phosphates': 3.15,
+            'fecal_coliform': 1.284
+        },
+        'sample_3': {
+            'water_temp': 15,
+            'water_temp_tool': 'Manual',
+            'air_temp': 32.756,
+            'air_temp_tool': 'Manual',
+            'dissolved_oxygen': 5.21,
+            'oxygen_tool': 'Vernier',
+            'pH': 7.2,
+            'pH_tool': 'Vernier',
+            'turbidity': 12.4,
+            'turbid_tool': 'Vernier',
+            'salinity': 6.784265422397,
+            'salt_tool': 'Manual',
+            'conductivity': 54.0,
+            'total_solids': 6.12,
+            'bod': 5.3,
+            'ammonia': 4.75,
+            'nitrite': 0.22,
+            'nitrate': 7.2,
+            'phosphates': 9.4,
+            'fecal_coliform': 6.23
+        },
+        'sample_4': {
+            'water_temp': 67,
+            'water_temp_tool': 'Vernier',
+            'air_temp': 11.56,
+            'air_temp_tool': 'Manual',
+            'dissolved_oxygen': 15.24,
+            'oxygen_tool': 'Vernier',
+            'pH': 4.2,
+            'pH_tool': 'Manual',
+            'turbidity': 2.5,
+            'turbid_tool': 'Manual',
+            'salinity': 6.54,
+            'salt_tool': 'Vernier',
+            'conductivity': 94.1,
+            'total_solids': 110.2,
+            'bod': 8.215,
+            'ammonia': 13.2,
+            'nitrite': 93.4,
+            'nitrate': 75.2,
+            'phosphates': 64.2,
+            'fecal_coliform': 57.2
+        },
+        'notes': 'All data completely made up for testing with.'
+    }
+    return render(request, 'streamwebs/datasheets/water_quality_view.html', {
+        'data': quality_data
+    })
+
+
+def water_quality_edit(request, site_slug):
+    site = {
+        'name': 'Test 1',
+        'slug': 'test1',
+        'site_type': 'steward',
+        'location': {'x': -122.31211423, 'y': 45.11231324}
+    }
+    return render(request, 'streamwebs/datasheets/water_quality_edit.html', {
+        'site': site
+    })
