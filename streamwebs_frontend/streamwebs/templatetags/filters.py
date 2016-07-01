0931723f4a8f1c8cb081db4e2a352c5530d4708e
Tristan Patch <patcht@osuosl.org>
2016-07-01 15:35:22 -0700

    Namespace the site references.

--- streamwebs_frontend/streamwebs/templatetags/filters.py
+++ streamwebs_frontend/streamwebs/templatetags/filters.py
@@ -7,3 +7,9 @@ register = Library()
 def strtoul(value):
     """Converts spaces into underlines in a string"""
     return value.replace(' ', '_')
+
+
+@register.filter
+def namespace(value):
+    """Converts URL references to namespaced references"""
+    return "streamwebs:" + value
