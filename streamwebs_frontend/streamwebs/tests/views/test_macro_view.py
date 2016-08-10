4077fc177c6b20aeaa1874b2a628c22ce1007da6
Alex Nguyen <alexlnguyen@gmail.com>
2016-08-10 13:28:39 -0700

    refactored and fixed macro view test thanks to leian

--- streamwebs_frontend/streamwebs/tests/views/test_macro_view.py
+++ streamwebs_frontend/streamwebs/tests/views/test_macro_view.py
@@ -4,43 +4,40 @@ from streamwebs.models import Site, Macroinvertebrates
 
 
 class MacroViewTestCase(TestCase):
+    def setUp(self):
+        self.site = Site.objects.create_site(
+            'Site Name', 'Site Type', 'site_slug'
+            )
+        self.macro = Macroinvertebrates.objects.create_macro(
+            self.site, 2, 3, True, True, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5,
+            6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8)
 
     def test_data_sheet_view(self):
-        site = Site.objects.create_site('Site Name', 'Site Type', 'site_slug')
-        macro = Macroinvertebrates.objects.create_macro(
-            site, 2, 3, True, True, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6,
-            7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8)
-
-        response = self.client.get(
+        self.response = self.client.get(
             reverse(
-                'streamwebs:macroinvertebrate',
-                kwargs={'data_id': macro.id,
-                        'site_slug': macro.site.id
+                'streamwebs:macroinvertebrate_view',
+                kwargs={'data_id': self.macro.id,
+                        'site_slug': self.macro.site.id
                         }
             )
         )
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(
-            response,
+            self.response,
             'streamwebs/datasheets/macroinvertebrate_view.html'
         )
 
     def test_data_sheet_view_content(self):
-        site = Site.objects.create_site('Site Name', 'Site Type', 'site_slug')
-        macro = Macroinvertebrates.objects.create_macro(
-            site, 2, 3, True, True, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6,
-            7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8)
-
         response = self.client.get(
             reverse(
-                'streamwebs:macroinvertebrate',
-                kwargs={'data_id': macro.id,
-                        'site_slug': macro.site.id
+                'streamwebs:macroinvertebrate_view',
+                kwargs={'data_id': self.macro.id,
+                        'site_slug': self.macro.site.id
                         }
             )
         )
 
-        self.assertContains(response, 'macroinvertebrates')
+        self.assertContains(response, 'Macroinvertebrates')
         self.assertContains(response, 'caddisfly')
         self.assertContains(response, 'mayfly')
         self.assertContains(response, 'stonefly')
