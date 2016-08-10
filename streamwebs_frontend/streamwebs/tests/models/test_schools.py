92710a033025f95c8add20d68b7156a7416a4fdc
Alex Nguyen <alexlnguyen@gmail.com>
2016-08-10 15:44:12 -0700

    First draft of schools model

---
+++ streamwebs_frontend/streamwebs/tests/models/test_schools.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from streamwebs.models import school
+from django.contrib.gis.db import models
+from django.apps import apps
+from itertools import chain
+
+
+class SiteTestCase(TestCase):
+
+    def setUp(self):
+        self.expected_fields = {
+            'name': models.CharField,
+            'school_type': models.CharField,
+            'address': models.CharField,
+            'city': models.CharField,
+            'zipcode': models.CharField,
+
+            'created': models.DateTimeField,
+            'modified': models.DateTimeField,
+
+            'id': models.AutoField,
+        }
+
+    def test_fields_exist(self):
+        model = apps.get_model('streamwebs', 'school')
+        for field, field_type in self.expected_fields.items():
+            self.assertEqual(
+                field_type, type(model._meta.get_field(field)))
+
+    def test_no_extra_fields(self):
+        # the following is equivalent to MyField._meta.get_all_field_names()
+        # which was deprecated in Django 1.9
+        fields = list(set(chain.from_iterable(
+            (field.name, field.attname) if hasattr(field, 'attname') else
+            (field.name,) for field in school._meta.get_fields()
+            if not (field.many_to_one and field.related_model is None)
+        )))
+        self.assertEqual(sorted(fields), sorted(self.expected_fields.keys()))
+
+    def test_optional_fields(self):
+        apps.get_model('streamwebs', 'school')
+        for field in self.optional_fields:
+            self.assertEqual(
+                school._meta.get_field(field).blank, True)
+
+    def test_create_and_mod_dates(self):
+        """When a new site is created, both date fields should be set"""
+        self.assertTrue(school._meta.get_field('modified').auto_now)
+        self.assertTrue(school._meta.get_field('created').auto_now_add)
