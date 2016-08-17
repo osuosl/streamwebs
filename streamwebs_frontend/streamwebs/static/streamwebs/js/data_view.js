a32192360d5a524967c8849864b587ccf06333c7
Alex Taylor <alex@subnomo.me>
2016-08-17 12:09:05 -0700

    Tweak dropdown

--- streamwebs_frontend/streamwebs/static/streamwebs/js/data_view.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/data_view.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
     $('select').material_select();
 
-    // Collapsible table rows
-    $('.collapse-header').click(function(){
-        if($(this).hasClass("collapsed")){
-            $(this).nextUntil('tr.collapse-header')
+    function collapse(obj) {
+        if($(obj).hasClass("collapsed")) {
+            $(obj).nextUntil('tr.collapse-header')
                 .find('td')
                 .parent()
                 .find('td > div')
-                .slideDown("fast", function(){
+                .slideDown("fast", function () {
                     var $set = $(this);
                     $set.replaceWith($set.contents());
                 });
-            $(this).removeClass("collapsed");
+            $(obj).removeClass("collapsed");
         } else {
-            $(this).nextUntil('tr.collapse-header')
+            $(obj).nextUntil('tr.collapse-header')
                 .find('td')
                 .wrapInner('<div style="display: block;" />')
                 .parent()
                 .find('td > div')
                 .slideUp("fast");
-            $(this).addClass("collapsed");
+            $(obj).addClass("collapsed");
         }
+    }
+
+    collapse($('.collapse-header'));
+
+    // Collapsible table rows
+    $('.collapse-header').click(function () {
+        collapse(this);
     });
 });
