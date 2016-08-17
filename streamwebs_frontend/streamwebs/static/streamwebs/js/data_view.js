2fb98bef660d4859fc1796ccc9600df9433ba1c9
Alex Taylor <alex@subnomo.me>
2016-08-17 09:41:21 -0700

    Adding dropdown for additional parameters

--- streamwebs_frontend/streamwebs/static/streamwebs/js/data_view.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/data_view.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('select').material_select();
+
+    // Collapsible table rows
+    $('.collapse-header').click(function(){
+        if($(this).hasClass("collapsed")){
+            $(this).nextUntil('tr.collapse-header')
+                .find('td')
+                .parent()
+                .find('td > div')
+                .slideDown("fast", function(){
+                    var $set = $(this);
+                    $set.replaceWith($set.contents());
+                });
+            $(this).removeClass("collapsed");
+        } else {
+            $(this).nextUntil('tr.collapse-header')
+                .find('td')
+                .wrapInner('<div style="display: block;" />')
+                .parent()
+                .find('td > div')
+                .slideUp("fast");
+            $(this).addClass("collapsed");
+        }
+    });
 });
