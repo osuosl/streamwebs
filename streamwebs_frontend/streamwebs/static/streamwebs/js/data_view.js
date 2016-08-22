f3081811dd1b6d6fb30e3cc4dd983b4694ba2b8a
Alex Taylor <alex@subnomo.me>
2016-08-22 13:16:30 -0700

    Improve 'additional parameters' collapsible
    Not sure how having a seperate table will affect water_quality_edit. We'll see.

--- streamwebs_frontend/streamwebs/static/streamwebs/js/data_view.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/data_view.js
@@ -1,32 +1,7 @@
 $(document).ready(function () {
     $('select').material_select();
-
-    function collapse(obj) {
-        if($(obj).hasClass("collapsed")) {
-            $(obj).nextUntil('tr.collapse-header')
-                .find('td')
-                .parent()
-                .find('td > div')
-                .slideDown("fast", function () {
-                    var $set = $(this);
-                    $set.replaceWith($set.contents());
-                });
-            $(obj).removeClass("collapsed");
-        } else {
-            $(obj).nextUntil('tr.collapse-header')
-                .find('td')
-                .wrapInner('<div style="display: block;" />')
-                .parent()
-                .find('td > div')
-                .slideUp("fast");
-            $(obj).addClass("collapsed");
-        }
-    }
-
-    collapse($('.collapse-header'));
-
-    // Collapsible table rows
-    $('.collapse-header').click(function () {
-        collapse(this);
+    
+    $('.collapse-btn').click(function () {
+        $('.collapse').toggleClass('uncollapse');
     });
 });
