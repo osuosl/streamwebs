a2703bc84a857ec3b76559b2cd2e206b02f21f5f
Alex Taylor <alex@subnomo.me>
2016-08-23 10:37:26 -0700

    Materialize water_quality_edit

--- streamwebs_frontend/streamwebs/static/streamwebs/js/data.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/data.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     $('select').material_select();
-    
+
+    $('.datepicker').pickadate({
+        format: 'yyyy-mm-dd'
+    });
+
     $('.collapse-btn').click(function () {
         $('.collapse').toggleClass('uncollapse');
     });
