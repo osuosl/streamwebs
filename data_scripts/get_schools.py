6fe1e32bdef4e90371bdc51087c7f8529ee99e34
Alex Nguyen <alexlnguyen@gmail.com>
2016-08-24 16:15:16 -0700

    finished get_schools.py

--- data_scripts/get_schools.py
+++ data_scripts/get_schools.py
@@ -13,57 +13,17 @@ application = get_wsgi_application()
 
 from streamwebs.models import School  # NOQA
 
-
-# Function to convert the (string) coordinates to a float value
-def num(x):
-    try:
-        return float(x)
-    except ValueError:
-        return x
-
-# Title, Post date, Updated date, Marker, Coordinates, Lat, Long
 with open('../csvs/schools_info.csv', 'r') as csvfile:
     sitereader = csv.DictReader(csvfile)
     for row in sitereader:
         school = School()
-        print row['School name']
-        print row['School type']
-        print row['Street']
-        print row['City']
-        print row['Province']
-        print row['Postal Code']
-        print row['Country']
-        print ""
 
-        """
         school.name = row['School name']
-        school.school_type = ['School type']
-        school.address =
-        school.city =
-        school.zipcode =
+        school.school_type = row['School type']
+        school.address = row['Street']
+        school.city = row['City']
+        school.province = row['Province'] + ", " + row['Country']
+        school.zipcode = row['Postal Code']
         school.save()
-        """
-        """
-
-        # Reassign colored marker to its corresponding site type
-        if row[3] == 'small blue':
-            site.site_type = 'Student Stewardship Project'
-        if row[3] == 'small orange':
-            site.site_type = 'Salmon Watch'
-
-        site.site_name = row[0]
-        site.created = row[1]
-        site.modified = row[2]
-
-        # Converting latitude and longitude to floats
-        lat = num(row[5])
-        lng = num(row[6])
-        if (lat != '' and lng != ''):  # Skip if there are no coordinates
-            pnt = Point(lat, lng)
-        site.location = pnt
-
-        # Save site data to django db
-        site.save()
-        """
 
 print "Data loaded."
