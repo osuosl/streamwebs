02d0e281dbd9e32a430f2101a082c6d91caf3999
Aileen Thai <thai@osuosl.org>
2016-09-01 13:53:57 -0700

    Tests for the view_view

--- streamwebs_frontend/streamwebs/tests/views/test_cc_view_view.py
+++ streamwebs_frontend/streamwebs/tests/views/test_cc_view_view.py
@@ -7,4 +7,86 @@ class ViewCanopyCoverTestCase(TestCase):
 
     def test_data_sheet_view(self):
         site = Site.test_objects.create_site('Site', 'Type')
+        canopy = Canopy_Cover.objects.create(
+            'School Name', '2016-09-01 11:54', site, 'Pleasant'
+        )
+        north = CC_Cardinal.test_objects.create_shade(
+            'North', True, True, False, True, False, False, False, True, False,
+            False, True, True, True, True, True, False, False, True, True,
+            False, False, True, True, False, False, True, 14, canopy
+        )
+        east = CC_Cardinal.test_objects.create_shade(
+            'East', False, False, False, True, True, False, False, True, False,
+            False, True, True, True, False, True, True, False, True, True,
+            False, False, True, True, False, False, True, 13, canopy
+        )
+        south =  CC_Cardinal.test_objects.create_shade(
+            'South', True, True, False, True, False, False, False, True, False,
+            False, True, False, True, True, True, False, False, True, True,
+            False, False, False, False, False, False, True, 11, canopy
+        )
+        west = CC_Cardinal.test_objects.create_shade(
+            'West', True, False, False, True, True, True, False, True, False,
+            False, True, True, False, True, True, False, False, True, True,
+            False, False, True, True, False, True, False, 14, canopy
+        )
 
+        response = self.client.get(
+            reverse(
+                'streamwebs:canopy_cover_view',
+                kwargs={'data_id': canopy.id,
+                        'site_slug': canopy.site.site_slug
+                        }
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'streamwebs/datasheets/canopy_cover_view.html'
+        )
+
+    def test_data_sheet_view_content(self):
+        site = Site.test_objects.create_site('Test', 'Type')
+        canopy = Canopy_Cover.objects.create(
+            'School Name', '2016-09-01 11:54', site, 'Pleasant'
+        )
+         north = CC_Cardinal.test_objects.create_shade(
+            'North', True, True, False, True, False, False, False, True, False,
+            False, True, True, True, True, True, False, False, True, True,
+            False, False, True, True, False, False, True, 14, canopy
+        )
+        east = CC_Cardinal.test_objects.create_shade(
+            'East', False, False, False, True, True, False, False, True, False,
+            False, True, True, True, False, True, True, False, True, True,
+            False, False, True, True, False, False, True, 13, canopy
+        )
+        south =  CC_Cardinal.test_objects.create_shade(
+            'South', True, True, False, True, False, False, False, True, False,
+            False, True, False, True, True, True, False, False, True, True,
+            False, False, False, False, False, False, True, 11, canopy
+        )
+        west = CC_Cardinal.test_objects.create_shade(
+            'West', True, False, False, True, True, True, False, True, False,
+            False, True, True, False, True, True, False, False, True, True,
+            False, False, True, True, False, True, False, 14, canopy
+        )
+
+        response = self.client.get(
+            reverse(
+                'streamwebs.canopy_cover_view',
+                kwargs={'data_id': canopy.id,
+                        'site_slug': site.site_slug
+                       }
+            )
+        )
+
+        self.assertTempaltedUsed(
+            response, 'streamwebs/datasheets/canopy_cover_view.html'
+        )
+        self.assertContains(response, 'Canopy Cover')
+        self.assertContains(response, 'School Name')
+        self.assertContains(response, 'September 1, 2016, 11:54 a.m.')
+        self.assertContains(response, 'Test')  # Site name
+        self.assertContains(response, 'North')
+        self.assertContains(response, 'East')
+        self.assertContains(response, 'South')
+        self.assertContains(response, 'West')
