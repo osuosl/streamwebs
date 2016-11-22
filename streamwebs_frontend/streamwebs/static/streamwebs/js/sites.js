25133d573134f934225e8669ccb87104aae04f46
Aileen Thai <thai@osuosl.org>
2016-11-21 16:12:56 -0800

    Add missing brackets

--- streamwebs_frontend/streamwebs/static/streamwebs/js/sites.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/sites.js
@@ -60,6 +60,7 @@ function initialize() {
 
         infoWindows[i] = new google.maps.InfoWindow({
             content: '<p><a href="javascript:submit_map(\'' + site.slug + '\')">' + site.name + '</a></p><p>' + site.description + '</p>',
+        });
 
         markerList[i].addListener('click', function () {
             for (let otherWindow of infoWindows) {
