d5ee59f8cbd15dd8e066c51fa775359e7b3c842c
Tristan Patch <patcht@osuosl.org>
2017-01-24 15:59:24 -0800

    Fix jQuery and ID problems.

--- streamwebs_frontend/streamwebs/static/streamwebs/js/data/edit/canopy_cover.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/data/edit/canopy_cover.js
@@ -1,6 +1,7 @@
 $(function () {
-    $("#total input").attr('readonly', 'true');
-    $("#total input").attr('num', 0);
+    const input = $("#total").find("input");
+    input.attr('readonly', 'true');
+    input.attr('num', 0);
 
     $(".canopy-square.hidden").mousedown(function(evt) {
       evt.preventDefault();
@@ -12,13 +13,13 @@ $(function () {
         square.toggleClass('shaded');
 
         if (square.hasClass('shaded')) {
-            let oldval = Number.parseInt($("#total input").attr('num'));
-            $("#total input").attr('num', oldval+1);
-            $("#total input").val(Math.floor((oldval+1) / 96 * 10000) / 100);
+            let oldval = Number.parseInt(input.attr('num'));
+            input.attr('num', oldval+1);
+            input.val(Math.floor((oldval+1) / 96 * 10000) / 100);
         } else {
-            let oldval = Number.parseInt($("#total input").attr('num'));
-            $("#total input").attr('num', oldval-1);
-            $("#total input").val(Math.floor((oldval-1) / 96 * 10000) / 100);
+            let oldval = Number.parseInt(input.attr('num'));
+            input.attr('num', oldval-1);
+            input.val(Math.floor((oldval-1) / 96 * 10000) / 100);
         }
 
         evt.preventDefault();
@@ -27,50 +28,50 @@ $(function () {
     $('#submit').click(function sendData() {
         let north = 0;
 
-        $('#canopy-north .canopy-square.shown').each((i, e) => {
+        $('#canopy-north').find('.canopy-square.shown').each((i, e) => {
             if ($(e).hasClass('shaded')) {
-                let num = Number.parseInt(e.id.slice(7)); // Drop off the 'square-'
+                let num = Number.parseInt(e.id.slice(9)); // Drop off the 'square-n-'
 
                 north |= (1 << num);
             }
         });
 
-        $('#north-form input').val(north);
+        $('#north-form').find('input').val(north);
 
         let west = 0;
 
-        $('#canopy-west .canopy-square.shown').each((i, e) => {
+        $('#canopy-west').find('.canopy-square.shown').each((i, e) => {
             if ($(e).hasClass('shaded')) {
-                let num = Number.parseInt(e.id.slice(7)); // Drop off the 'square-'
+                let num = Number.parseInt(e.id.slice(9)); // Drop off the 'square-n-'
 
                 west |= (1 << num);
             }
         });
 
-        $('#west-form input').val(west);
+        $('#west-form').find('input').val(west);
 
         let east = 0;
 
-        $('#canopy-east .canopy-square.shown').each((i, e) => {
+        $('#canopy-east').find('.canopy-square.shown').each((i, e) => {
             if ($(e).hasClass('shaded')) {
-                let num = Number.parseInt(e.id.slice(7)); // Drop off the 'square-'
+                let num = Number.parseInt(e.id.slice(9)); // Drop off the 'square-n-'
 
                 east |= (1 << num);
             }
         });
 
-        $('#east-form input').val(east);
+        $('#east-form').find('input').val(east);
 
         let south = 0;
 
-        $('#canopy-south .canopy-square.shown').each((i, e) => {
+        $('#canopy-south').find('.canopy-square.shown').each((i, e) => {
             if ($(e).hasClass('shaded')) {
-                let num = Number.parseInt(e.id.slice(7)); // Drop off the 'square-'
+                let num = Number.parseInt(e.id.slice(9)); // Drop off the 'square-n-'
 
                 south |= (1 << num);
             }
         });
 
-        $('#south-form input').val(south);
+        $('#south-form').find('input').val(south);
     });
 });
