a1b585989b14ccfef2901d40dce1f9123b301f9f
Aileen Thai <thai@osuosl.org>
2017-02-28 15:39:18 -0800

    Use django-queryset-csv to export data

--- streamwebs_frontend/streamwebs/views.py
+++ streamwebs_frontend/streamwebs/views.py
@@ -26,70 +26,25 @@ from streamwebs.models import (
     PhotoPointImage, Soil_Survey, Resource, School)
 
 from datetime import datetime
-#from djqscsv import write_csv
+from djqscsv import render_to_csv_response
 import json
 import copy
 import csv
 
 
-class Echo(object):
-    """An object that implements just the write method of the file-like
-    interface"""
-    def write(self, value):
-        """Write the value by returning it, instead of storing in a buffer."""
-        return value
-
-
-#def stream_macros(request, site_slug):
-#    """A view that streams a large CSV file."""
-#    # Generate a seqence of rows. The range is based on the maximum number of
-#    # rows that can be handled by a single sheet in most spreadsheet apps
-#
-#    #if request.method == 'GET':
-#    #macros = Macroinvertebrates.objects.select_related(site_slug=site_slug)
-#    macros = Macroinvertebrates.objects.all()
-#
-#    row = (["Row{}".format(idx), str(idx)] for idx in range(65536))
-#    pseudo_buffer = Echo()
-#    writer = csv.writer(pseudo_buffer)
-#    response = StreamingHttpResponse((writer.writerow(row) for row in rows),
-#                                       content_type="text/csv")
-#    response['Content-Disposition'] = 'attachment; filename="macros.csv"'
-#    return response
-
 def export_macros(request, site_slug):
     site = Site.objects.get(site_slug=site_slug)
-    qs = Macroinvertebrates.objects.filter(site_id=site.id)
-
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="macros.csv"'
-
-    writer = csv.writer(response)
-    writer.writerow(['test', 'foo', 'bar', 'baz', 'fizz'])
-    writer.writerow(['b', 'r', 'v', 'a', 'here']) #, '"quotes"']) -- quoted items seem to break format?
-
-    return response
-
-#    outfile_path = site.name = '.csv'
-#
-#    model = qs.model
-#    writer = csv.writer(open(outfile_path, 'w'))
-#
-#    headers = []
-#    for field in model._meta.fields:
-#        headers.append(field.name)
-#    writer.writerow(headers)
-#
-#    for obj in qs:
-#        row = []
-#        for field in headers:
-#            val = getattr(obj, field)
-#            if callable(val):
-#                val = val()
-#            if type(val) == unicode:
-#                val = val.encode("utf-8")
-#            row.append(val)
-#        writer.writerow(row)
+    macros = Macroinvertebrates.objects.filter(site_id=site.id)
+    data = macros.values(
+        'school', 'date_time', 'site__site_name', 'weather', 'time_spent',
+        'num_people', 'water_type', 'notes', 'caddisfly', 'mayfly',
+        'riffle_beetle', 'stonefly', 'water_penny', 'dobsonfly',
+        'sensitive_total', 'clam_or_mussel', 'crane_fly', 'crayfish',
+        'damselfly', 'dragonfly', 'scud', 'fishfly', 'alderfly', 'mite',
+        'somewhat_sensitive_total', 'aquatic_worm', 'blackfly', 'leech',
+        'midge', 'snail', 'mosquito_larva', 'tolerant_total', 'wq_rating'
+        )
+    return render_to_csv_response(data)
 
 
 def _timestamp(dt):
