bf4ff2910766357b3a989590c26018b635e3bc7f
leian7 <leian@osuosl.org>
2017-04-10 12:26:10 -0700

    implement/test basic funct with template

--- streamwebs_frontend/streamwebs/tests/views/test_admin_promo_view.py
+++ streamwebs_frontend/streamwebs/tests/views/test_admin_promo_view.py
@@ -12,6 +12,9 @@ class AdminPromoTestCase(TestCase):
         admins = Group.objects.get(name='admin')
         can_promo = Permission.objects.get(codename='can_promote_users')
 
+        self.user1 = User.objects.get(pk=2)
+        self.user2 = User.objects.get(pk=7)
+
         # super admin (level: Renee) with promotion permission
         self.superAdmin = User.objects.create_user('superAdmin',
                                                    'super@example.com',
@@ -70,3 +73,87 @@ class AdminPromoTestCase(TestCase):
         self.assertTemplateNotUsed(response,
                                    'streamwebs/admin/user_promo.html')
         self.client.logout()
+
+    def test_view_with_bad_blank_data(self):
+        """If user submits bad (blank) form, form errors displayed"""
+        self.client.login(username='superAdmin', password='superpassword')
+        response = self.client.post(reverse('streamwebs:user_promo'), {})
+        self.assertFormError(response, 'promo_form', 'users',
+                             'This field is required.')
+        self.assertFormError(response, 'promo_form', 'perms',
+                             'This field is required.')
+        self.client.logout()
+
+    def test_add_users_to_admin_group(self):
+        """Tests that users can be added to the admin group"""
+        self.client.login(username='superAdmin', password='superpassword')
+
+        self.assertFalse(self.user1.groups.filter(name='admin').exists())
+        self.assertFalse(self.user2.groups.filter(name='admin').exists())
+
+        response = self.client.post(reverse('streamwebs:user_promo'), {
+            'users': (self.user1.id, self.user2.id),
+            'perms': 'add_admin',
+            }
+        )
+        self.assertTrue(self.user1.groups.filter(name='admin').exists())
+        self.assertTrue(self.user2.groups.filter(name='admin').exists())
+
+    def test_remove_users_from_admin_group(self):
+        """Tests that users can be removed from the admin group"""
+        self.client.login(username='superAdmin', password='superpassword')
+
+        response = self.client.post(reverse('streamwebs:user_promo'), {
+            'users': (self.user1.id),
+            'perms': 'del_admin',
+            }
+        )
+        self.assertFalse(self.user1.groups.filter(name='admin').exists())
+
+    def test_add_stats_perm_for_users(self):
+        """Tests that users can be granted the can_view_stats permission"""
+        self.client.login(username='superAdmin', password='superpassword')
+
+        response = self.client.post(reverse('streamwebs:user_promo'), {
+            'users': (self.user1.id),
+            'perms': 'add_stats',
+            }
+        )
+        self.assertTrue(self.user1.has_perm('streamwebs.can_view_stats'))
+
+    def test_remove_stats_perm_for_users(self):
+        """Tests that users can have the can_view_stats permission revoked"""
+        self.client.login(username='superAdmin', password='superpassword')
+
+        response = self.client.post(reverse('streamwebs:user_promo'), {
+            'users': (self.user1.id, self.user2.id),
+            'perms': 'del_stats',
+            }
+        )
+        self.assertFalse(self.user1.has_perm('streamwebs.can_view_stats'))
+        self.assertFalse(self.user2.has_perm('streamwebs.can_view_stats'))
+
+    def test_add_upload_perm_for_users(self):
+        """Tests that users can be granted the can_upload_resources perm"""
+        self.client.login(username='superAdmin', password='superpassword')
+
+        response = self.client.post(reverse('streamwebs:user_promo'), {
+            'users': (self.user1.id),
+            'perms': 'add_upload',
+            }
+        )
+        self.assertTrue(self.user1.has_perm('streamwebs.can_upload_resources'))
+
+    def test_remove_upload_perm_for_users(self):
+        """Tests that users can have the can_upload_stats permission revoked"""
+        self.client.login(username='superAdmin', password='superpassword')
+
+        response = self.client.post(reverse('streamwebs:user_promo'), {
+            'users': (self.user1.id, self.user2.id),
+            'perms': 'del_upload',
+            }
+        )
+        self.assertFalse(self.user1.has_perm(
+            'streamwebs.can_upload_resources'))
+        self.assertFalse(self.user2.has_perm(
+            'streamwebs.can_upload_resources'))
