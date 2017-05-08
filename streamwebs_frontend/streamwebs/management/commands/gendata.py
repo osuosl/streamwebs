c0607124f5594523232975e4e311ee3360f97ce9
Morgan Patch <patchm@osuosl.org>
2017-05-08 14:55:46 -0700

    Generate canopy cover data.

--- streamwebs_frontend/streamwebs/management/commands/gendata.py
+++ streamwebs_frontend/streamwebs/management/commands/gendata.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from streamwebs.models import Site, Water_Quality, WQ_Sample,\
-    Macroinvertebrates
+    Macroinvertebrates, Canopy_Cover
 
 from datetime import date, datetime
 from random import randint, uniform, betavariate as beta
@@ -58,19 +58,20 @@ class Command(BaseCommand):
 
         if type == 'wq' or type == 'all':
             print('Generating Water Quality data...')
-            for month in range(1, 13):
+            for mon in range(1, 13):
                 for i in range(randint(50, 100)):
-                    wq = Water_Quality.objects.create(site=s,
-                                                      date=date(2017, month, 1),
-                                                      DEQ_dq_level='C',
-                                                      school=None,
-                                                      latitude=-120,
-                                                      longitude=45,
-                                                      fish_present='True',
-                                                      live_fish=3,
-                                                      dead_fish=2,
-                                                      water_temp_unit='Celsius',
-                                                      air_temp_unit='Celsius')
+                    wq = Water_Quality\
+                        .objects.create(site=s,
+                                        date=date(2017, mon, 1),
+                                        DEQ_dq_level='C',
+                                        school=None,
+                                        latitude=-120,
+                                        longitude=45,
+                                        fish_present='True',
+                                        live_fish=3,
+                                        dead_fish=2,
+                                        water_temp_unit='Celsius',
+                                        air_temp_unit='Celsius')
                     wq.save()
 
                     for n in range(4):
@@ -139,6 +140,47 @@ class Command(BaseCommand):
 
         if type == 'cc' or type == 'all':
             print('Generating Canopy Cover data...')
+            zero = Canopy_Cover.objects.create(school=None,
+                                               date_time=datetime(2017, 4, 1),
+                                               site=s,
+                                               weather='Sunny',
+                                               north_cc=0,
+                                               west_cc=0,
+                                               east_cc=0,
+                                               south_cc=0,
+                                               est_canopy_cover=0)
+            zero.save()
+
+            for i in range(2, 30):
+                # 16777215 = 0b111111111111111111111110
+                north_cc = randint(1, 16777214)
+                west_cc = randint(1, 16777214)
+                east_cc = randint(1, 16777214)
+                south_cc = randint(1, 16777214)
+                total = bin(north_cc).count('1') + bin(west_cc).count('1') +\
+                    bin(east_cc).count('1') + bin(south_cc).count('1')
+                cc = Canopy_Cover\
+                    .objects.create(school=None,
+                                    date_time=datetime(2017, 4, i),
+                                    site=s,
+                                    weather='Sunny',
+                                    north_cc=north_cc,
+                                    west_cc=west_cc,
+                                    east_cc=east_cc,
+                                    south_cc=south_cc,
+                                    est_canopy_cover=total)
+                cc.save()
+
+            all = Canopy_Cover.objects.create(school=None,
+                                              date_time=datetime(2017, 4, 30),
+                                              site=s,
+                                              weather='Sunny',
+                                              north_cc=16777215,
+                                              west_cc=16777215,
+                                              east_cc=16777215,
+                                              south_cc=16777215,
+                                              est_canopy_cover=96)
+            all.save()
 
         if type == 'transect' or type == 'all':
             print('Generating Riparian Transect data...')
