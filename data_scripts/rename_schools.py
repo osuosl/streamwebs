4313eb8f619227d6ac6172998c149d2f3890e2c9
Aileen Thai <thai@osuosl.org>
2017-05-26 16:36:14 -0700

    Not perfect, but at a working state

--- data_scripts/rename_schools.py
+++ data_scripts/rename_schools.py
@@ -11,7 +11,7 @@ proj_path = "../streamwebs_frontend/"
 sys.path.append(proj_path)
 application = get_wsgi_application()
 
-from streamwebs.models import School, SchoolRelations  # NOQA
+from streamwebs.models import School, SchoolRelations # NOQA
 
 if os.path.isdir("../streamwebs_frontend/sw_data/"):
     # Update this at some point
@@ -20,6 +20,13 @@ else:
     # datafile = '../csvs/schools_info.csv'
     datafile = '../csvs/active_schools.csv'
 
+
+# If default non-value doesn't exist, create it
+unnamed = School.objects.update_or_create(
+    name='School unspecified', school_type='Unknown',
+    address='Unknown', city='Unknown', province='Unknown',
+    zipcode='Unknown'
+)
 
 with open(datafile, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -55,28 +62,33 @@ with open(datafile, 'r') as csvfile:
               qry.name = str(qry) + ' (' + str(qry.city) + ')'
               qry.save()
         else:
-            if school.exists() and school.count != 0:
-                if row['Uid'] != '':
-                    uid = row['Uid']
-                else:
-                    uid = None
-                
-                related_school =  school.first()
+            if row['Uid'] != '':
+                uid = row['Uid']
+            else:
+                uid = None
 
-                relations = SchoolRelations.objects.update_or_create(
-                    uid=uid, school=related_school
-                )
+            # Make relation between uid and school if it exists
+            # otherwise, assign a default non-value for school            
+            if school.exists() and school.count != 0:
+                related_school = school.first()
+            else:
+                related_school = School.objects.get(name='School unspecified')
+
+            relations = SchoolRelations.objects.update_or_create(
+                uid=uid, school=related_school
+            )
 
 # Manually assigning uid-to-school relations for renamed duplicates...
 adams_uids = [1071, 1026, 188, 185, 182]
-other_uids = [1781, 163, 1726, 1671, 1101, 608, 83]
+other_uids = [1781, 163, 1726, 1671, 1101, 608, 83, 663]
 assoc_schools = ['Cascade Middle School (Bend)',
                  'Hoover Elementary School (Salem)',
                  'Hoover Elementary School (Corvallis)',
                  'Howard Elementary School (Eugene)',
                  'Wilson Elementary School (Medford)',
                  'Evergreen Elementary School (Redmond)',
-                 'Roosevelt Elementary School (Klamath Falls)']
+                 'Roosevelt Elementary School (Klamath Falls)',
+                 'Butte Falls Charter School (Butte Falls)']  # Ehhhhh
 
 for i in range(len(other_uids)):
     uid = other_uids[i]
--- data_scripts/set_schools.py
+++ data_scripts/set_schools.py
@@ -13,7 +13,7 @@ proj_path = "../streamwebs_frontend/"
 sys.path.append(proj_path)
 application = get_wsgi_application()
 
-from streamwebs.models import Soil_Survey, School  # NOQA
+from streamwebs.models import Soil_Survey, School, SchoolRelations  # NOQA
 
 if os.path.isdir("../streamwebs_frontend/sw_data/"):
     datafile = '../sw_data/active_schools.csv'
@@ -25,29 +25,35 @@ with open(datafile, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         #if row['School'] != '':
-        #print(row['School'])
+        #print ('??', row['School'])
 
         # Query Schools for ID
         # school = School.objects.get(nid=row['Nid'])
 
         if row['Uid'] != '':
+            #print (row['Uid'])
             # Check if any soil data sheets were submitted by this user/school
             if Soil_Survey.objects.filter(uid=row['Uid']).exists():
-              # Update School field for soil survey
+                # Update School field for soil survey
                 soil_sheets = []
                 soil_survey = Soil_Survey.objects.filter(uid=row['Uid'])
+
+                # Nothing appears to be happening???
+                #print ('Got here ' + str(soil_survey))
+                relation = SchoolRelations.objects.get(uid=row['Uid'])
 
                 for each in soil_survey:
                     soil_sheets.append(each.id)
 
+                # MAKE RELATIONS BETWEEN UID AND SCHOOL
                 for each in soil_sheets:
                     #print ('Soil site: ' + str(soil_survey[i]) + '\t' + str(school[0]))
                     soil = Soil_Survey.objects.get(id=each)
                     #print("test: " + str(type(soil_survey[i])))
-                    #print("test school: " + str(type(school_key)))
-                    soil.school = school.id
-                    print (soil)
-                    #soil.save()
+                    print("type: " + str(type(relation.school)))
+                    soil.school = relation.school
+                    #print (soil, 'relation: ' + str(relation.school))
+                    soil.save()
 #        else:
 #            print("User: " + row['Uid']  + " not affiliated with school")
 #        except streamwebs.models.MultipleObjectsReturned:
