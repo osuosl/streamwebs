72c812efe4fbc2e421aecbc3123332a65d2b74b4
Aileen Thai <thai@osuosl.org>
2017-06-14 14:35:23 -0700

    Fix bug - needed to run the rename script twice due to duplicate renaming

--- data_scripts/rename_schools.py
+++ data_scripts/rename_schools.py
@@ -15,10 +15,8 @@ application = get_wsgi_application()
 from streamwebs.models import School, SchoolRelations # NOQA
 
 if os.path.isdir("../streamwebs_frontend/sw_data/"):
-    # Update this at some point
     datafile = '../sw_data/active_schools.csv'
 else:
-    # datafile = '../csvs/schools_info.csv'
     datafile = '../csvs/active_schools.csv'
 
 
@@ -74,37 +72,8 @@ with open(datafile, 'r') as csvfile:
             else:
                 related_school = School.objects.get(name='School unspecified')
 
-            relations = SchoolRelations.objects.update_or_create(
+            relations, created = SchoolRelations.objects.update_or_create(
                 uid=uid, school=related_school
             )
-
-# Must sleep for two seconds to allow django enough time to create relations
-time.sleep(2)
-
-# Manually assigning uid-to-school relations for renamed duplicates...
-adams_uids = [1071, 1026, 188, 185, 182]
-other_uids = [1781, 163, 1726, 1671, 1101, 608, 83]
-assoc_schools = ['Cascade Middle School (Bend)',
-                 'Hoover Elementary School (Salem)',
-                 'Hoover Elementary School (Corvallis)',
-                 'Howard Elementary School (Eugene)',
-                 'Wilson Elementary School (Medford)',
-                 'Evergreen Elementary School (Redmond)',
-                 'Roosevelt Elementary School (Klamath Falls)']
-
-for i in range(len(other_uids)):
-    uid = other_uids[i]
-    related_school = School.objects.get(name=assoc_schools[i])
-    relation = SchoolRelations.objects.get(uid=uid)
-    relation.school = related_school
-    relation.save()
-
-# Make one query since the following uids are associated w/ the same school
-related_school = School.objects.get(name='Adams Elementary School (Corvallis)')
-for each in adams_uids:
-    uid = each
-    relation = SchoolRelations.objects.get(uid=uid)
-    relation.school = related_school
-    relation.save()
 
 print "Duplicates renamed, uid-to-school relations made."
