500ed7393e8e55b6ec19ef035192ad38995fc289
jerrypeng0112 <jerrypeng0112@hotmail.com>
2017-07-26 16:22:20 -0700

    Added axes, need to fix bars

--- streamwebs_frontend/streamwebs/static/streamwebs/js/data/view/riparian_transect.js
+++ streamwebs_frontend/streamwebs/static/streamwebs/js/data/view/riparian_transect.js
@@ -1,6 +1,6 @@
 var zonesData = window.zones_json;
-var margin = {top: 10, right: 10, bottom:40, left:40};
-var dimension = {maxWidth: 800, HtoWRatio: 0.6};
+var margin = {top: 10, right: 10, bottom:10, left:28};
+var dimension = {maxWidth: 800, HtoWRatio: 0.6, graphZoneMargin: 3};
 var zoneNames = [
                     'Zone 1 (20\')',
                     'Zone 2 (40\')',
@@ -20,38 +20,49 @@ var createGraph = function createGraph() {
     $('div#rip-graph').width(containerWidth).height(containerHeight);
 
     var svg = d3.select('div#rip-graph').append('svg')
-              .attr('width', "100%")
-              .attr('height', "100%");
+                .attr('width', "100%")
+                .attr('height', "100%");
 
-    var g = svg.append('g')
-            .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    var graph = svg.append('g')
+                   .attr('transform', 'translate(' + 0 + ', ' + 0 + ')');
 
+    createAxis(graph, graphWidth, graphHeight);
+    createBars(graph, graphWidth, graphHeight)
+}
+
+var createAxis = function createAxis(graphContainer, graphWidth, graphHeight) {
     var xBackground = d3.scaleLinear()
-                      .domain([0, 1])
-                      .range([0, graphWidth]);
+                        .domain([0, 1])
+                        .range([0, graphWidth]);
+
     var x = d3.scaleOrdinal()
-            .domain(zoneNames)
-            .range([50,150,250,350,450]);
+              .domain(zoneNames)
+              .range([50,150,250,350,450]);
 
     var y = d3.scaleLinear()
-            .domain([0, getXMax()])
-            .range([graphHeight, 0]);
+              .domain([0, getXMax()])
+              .range([graphHeight, 0]);
 
     var xBackgroundAxis = d3.axisBottom(xBackground)
-                          .tickFormat("")
-                          .ticks(6);
+                            .ticks(6)
+                            .tickFormat("");
+
     var xAxis = d3.axisBottom(x)
-                .ticks(6)
-                .tickSize(0);
-    var yAxis = d3.axisLeft(y)
-                .tickSize(10);
-    svg.append('g')
-       .call(yAxis)
-       .attr('transform', 'translate(' + margin.left + ', ' + 0 + ')');
-    svg.append('g')
-       .call(xAxis);
-    svg.append('g')
-       .call(xBackgroundAxis);
+                  .tickSize(0);
+
+    var yAxis = d3.axisLeft(y);
+
+    graphContainer.append('g')
+                  .call(xBackgroundAxis)
+                  .attr('transform', 'translate(' + margin.left + ', ' + graphHeight + ')');
+
+    graphContainer.append('g')
+                  .call(xAxis)
+                  .attr('transform', 'translate(' + margin.left + ', ' + graphHeight + ')');
+
+    graphContainer.append('g')
+                  .call(yAxis)
+                  .attr('transform', 'translate(' + margin.left + ', ' + 0 + ')');
 }
 
 var getXMax = function getXMax() {
@@ -63,6 +74,16 @@ var getXMax = function getXMax() {
         max = Math.max(max, zoneMax);
     });
     return Math.ceil(max / 10) + max;
+}
+
+var createBars = function createBars(graphContainer, graphWidth, graphHeight) {
+    /*graphContainer.append('g').selectAll('g')
+        .data(zonesData)
+        .enter().append('g')
+            .attr('transform', function(d, i) {
+
+            })*/
+
 }
 
 var clearGraph = function clearGraph() {
