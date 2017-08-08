180754f711e003e263d39cd9b30661cde351730a
jerrypeng0112 <jerrypeng0112@hotmail.com>
2017-08-08 14:29:15 -0700

    Fix flake8

--- streamwebs_frontend/streamwebs_frontend/urls.py
+++ streamwebs_frontend/streamwebs_frontend/urls.py
@@ -25,11 +25,15 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('streamwebs.urls')),
 
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^password_reset/$', auth_views.password_reset,
+        name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}' +
+        '-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete,
+        name='password_reset_complete'),
 ]
 
 if settings.DEBUG is True:
