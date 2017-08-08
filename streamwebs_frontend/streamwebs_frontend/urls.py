1f4603ba759d12692513c6696e9f667aef382f46
jerrypeng0112 <jerrypeng0112@hotmail.com>
2017-08-07 17:24:56 -0700

    Added password Reset function

--- streamwebs_frontend/streamwebs_frontend/urls.py
+++ streamwebs_frontend/streamwebs_frontend/urls.py
@@ -18,12 +18,18 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('streamwebs.urls')),
+
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
 
 if settings.DEBUG is True:
