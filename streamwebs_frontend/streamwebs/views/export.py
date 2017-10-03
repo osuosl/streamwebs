29d915a7b815520fa304e672930c8631878aa203
jerrypeng0112 <jerrypeng0112@hotmail.com>
2017-10-02 17:35:51 -0700

    fixed test and travis

--- streamwebs_frontend/streamwebs/views/export.py
+++ streamwebs_frontend/streamwebs/views/export.py
@@ -224,8 +224,9 @@ def export_cc(request, site_slug):
 def export_soil(request, site_slug):
     site = Site.objects.get(site_slug=site_slug)
     soil = Soil_Survey.objects.filter(site_id=site.id).values(
-        'school__name', 'date_time', 'site__site_name', 'weather', 'landscape_pos',
-        'cover_type', 'land_use', 'soil_type', 'distance', 'site_char'
+        'school__name', 'date_time', 'site__site_name', 'weather',
+        'landscape_pos', 'cover_type', 'land_use', 'soil_type', 'distance',
+        'site_char'
     )
     return render_to_csv_response(
         soil, field_header_map={
