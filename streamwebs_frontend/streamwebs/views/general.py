40c51d16229984d5fa0e345d2bcd038f519be4f4
baileysingleton <baileysingleton@github.com>
2017-10-13 14:21:36 -0700

    hopefully this toast pops up for windows. and i fixed the redirect of login to index

--- streamwebs_frontend/streamwebs/views/general.py
+++ streamwebs_frontend/streamwebs/views/general.py
@@ -460,10 +460,7 @@ def user_login(request):
         # url
         if user:
             login(request, user)
-            if redirect_to != '':
-                return HttpResponseRedirect(redirect_to)
-            else:
-                return redirect(reverse('streamwebs:index'))
+            return redirect(reverse('streamwebs:index'))
 
         # otherwise, if the user is invalid, flash a message and return them to
         # login while remembering which page to redirect them to if they login
