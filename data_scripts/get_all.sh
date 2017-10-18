2f347afef5b3d2eded9336d07976e684384d16f5
Lance Albertson <lance@osuosl.org>
2017-10-18 13:51:24 -0700

    Script to load photo points

--- data_scripts/get_all.sh
+++ data_scripts/get_all.sh
@@ -13,6 +13,7 @@ set -e
 ./get_cc.py
 ./get_soil.py
 ./get_camera_points.py
+./get_photo_points.py
 # Run twice so that relations are created for renamed schools
 ./rename_schools.py
 ./rename_schools.py
