d324fd3d535b4686b0f0f777a6fe2f3132808aa7
Lance Albertson <lance@osuosl.org>
2017-11-07 13:59:59 -0800

    Wait until postgres starts before running migrations

--- dockerfiles/startup.sh
+++ dockerfiles/startup.sh
@@ -2,6 +2,12 @@
 
 trap /home/centos/streamwebs/dockerfiles/cleanup.sh EXIT
 bower install --allow-root
+export PGPASSWORD=$POSTGRES_PASSWORD
+echo "Waiting for postgres to start.."
+until $(psql -h postgres_host -U $POSTGRES_USER streamwebs \
+  -c 'SELECT PostGIS_full_version();' 2> /dev/null | grep -q POSTGIS) ; do
+	sleep 1
+done
 yes "yes" | python /home/centos/streamwebs/streamwebs_frontend/manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.filter(email='streamwebs@osuosl.org').delete(); User.objects.create_superuser('admin', 'streamwebs@osuosl.org', 'admin')" | python streamwebs_frontend/manage.py shell
 python /home/centos/streamwebs/streamwebs_frontend/manage.py runserver 0.0.0.0:8000
